82aee4b99e6a9e8ac2e156a194881e981de6acab
klos71 <klos71.jonas@gmail.com>
2023-03-31 14:27:39 +0200

    fixed wasm build and usage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,10 @@
+import { WASMContextProvider } from '@/context/WASM';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <WASMContextProvider>
+            <Component {...pageProps} />
+        </WASMContextProvider>
+    );
 }
