7efef6c2279c697d29e563416b2cb270da850b12
klos71 <klos71.jonas@gmail.com>
2023-04-01 14:32:12 +0200

    added ascii to binary

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,22 @@
 import { WASMContextProvider } from '@/context/WASM';
 import type { AppProps } from 'next/app';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    html,body,#__next {
+        margin: 0;
+        padding: 0px;
+        height: 100%;
+        width: 100%;
+        font-family: sans-serif;
+        scroll-behavior: smooth;
+    }
+`;
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <WASMContextProvider>
+            <GlobalStyle />
             <Component {...pageProps} />
         </WASMContextProvider>
     );
